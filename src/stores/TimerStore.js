f3ce2c279ff314023908d56d1573f2ef318d0a5d
zielww <118595018+zielww@users.noreply.github.com>
2025-07-31 00:16:12 +0800

    refactored code and ui

--- src/stores/TimerStore.js
+++ src/stores/TimerStore.js
@@ -1,66 +1,50 @@
-import { defineStore } from 'pinia';
-
-let interval;
+import { defineStore } from 'pinia'
 
 export let useTimerStore = defineStore('timer', {
   state: () => ({
-    startingTime: 10000,
-    endTime: 0,
-    minutes: "00",
-    seconds: "00",
+    startingTime: 120_000,
+    timeLeft: 120_000,
+    intervalId: null,
     timeRunning: false,
+    startedAt: 0,
   }),
 
-  actions: {
-    fill() {
-      this.$patch({
-        minutes: this.getMinutes,
-        seconds: this.getSeconds,
-      })
+  getters: {
+    totalSeconds() {
+      return Math.floor(this.timeLeft / 1000);
     },
 
+    minutes() {
+      return String(Math.floor(this.totalSeconds / 60)).padStart(2, '0');
+    },
+
+    seconds() {
+      return String(this.totalSeconds % 60).padStart(2, '0');
+    },
+  },
+
+  actions: {
     startTimer() {
       if (this.timeRunning) return;
 
-      this.stopTimer();
-      interval = setInterval(() => {
-        if (this.startingTime > 0) {
-          this.startingTime -= 1000,
-          this.fill();
-        }
-      }, 1000);
-
+      this.startedAt = Date.now();
       this.timeRunning = true;
+
+      this.intervalId = setInterval(() => {
+        const elapsed = Date.now() - this.startedAt;
+        this.timeLeft = Math.max(0, this.startingTime - elapsed);
+        if (this.timeLeft === 0) this.stopTimer();
+      }, 250);
     },
 
     stopTimer() {
-      clearInterval(interval);
+      clearInterval(this.intervalId);
       this.timeRunning = false;
     },
 
     resetTimer() {
       this.stopTimer();
-      this.startingTime = 120000;
-      this.fill();
-    },
-
-    // Add Padding to the Text
-    padTime(time) {
-      return time.toString().padStart(2, "0");
+      this.timeLeft = this.startingTime;
     },
   },
-
-  getters: {
-    getTotalSeconds() {
-      return Math.floor(this.startingTime / 1000);
-    },
-
-    getMinutes() {
-      return this.padTime(Math.floor(this.getTotalSeconds / 60));
-    },
-
-    getSeconds() {
-      return this.padTime(this.getTotalSeconds % 60);
-    },
-  }
 })
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -1,18 +1,11 @@
 <script setup>
-import { useTimerStore } from '@/stores/TimerStore.js'
+import { useTimerStore } from '@/stores/TimerStore.js';
 
-const timer = useTimerStore()
+const timer = useTimerStore();
 
-timer.fill();
-
-function buttonLabel() {
-  return timer.timeRunning ? 'Stop' : 'Start';
-}
-
-function buttonAction() {
+function toggle() {
   return timer.timeRunning ? timer.stopTimer() : timer.startTimer();
 }
-
 </script>
 
 <template>
@@ -30,8 +23,13 @@ function buttonAction() {
     <div class="bg-holder h-2 fixed w-full"></div>
 
     <div class="fixed bottom-0 pb-10 flex gap-x-5">
-      <button  @click="buttonAction" class="cursor-pointer bg-holder py-2 px-4 rounded-md hover:opacity-80"> {{ buttonLabel() }}</button>
-      <button @click="timer.resetTimer()" class="cursor-pointer bg-holder py-2 px-4 rounded-md hover:opacity-80">Reset</button>
+      <button  @click="toggle" class="cursor-pointer bg-holder p-2 rounded-lg hover:opacity-80">
+        <svg v-if="!timer.timeRunning" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b7b7b7"><path d="M320-200v-560l440 280-440 280Zm80-280Zm0 134 210-134-210-134v268Z"/></svg>
+        <svg v-if="timer.timeRunning" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b7b7b7"><path d="M520-200v-560h240v560H520Zm-320 0v-560h240v560H200Zm400-80h80v-400h-80v400Zm-320 0h80v-400h-80v400Zm0-400v400-400Zm320 0v400-400Z"/></svg>
+      </button>
+      <button @click="timer.resetTimer()" class="cursor-pointer bg-holder p-2 rounded-lg hover:opacity-80">
+        <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#b7b7b7"><path d="M440-122q-121-15-200.5-105.5T160-440q0-66 26-126.5T260-672l57 57q-38 34-57.5 79T240-440q0 88 56 155.5T440-202v80Zm80 0v-80q87-16 143.5-83T720-440q0-100-70-170t-170-70h-3l44 44-56 56-140-140 140-140 56 56-44 44h3q134 0 227 93t93 227q0 121-79.5 211.5T520-122Z"/></svg>
+      </button>
     </div>
   </main>
 </template>
